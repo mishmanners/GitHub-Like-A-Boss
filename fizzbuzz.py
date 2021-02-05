981962f9010928c14673f39c0fe29f312fbe5a29
MishManners <36594527+MishManners@users.noreply.github.com>
2021-02-05 19:03:28 +1100

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,24 @@
+// FizzBuzz, called the constant donut... find me on my Twitch channel (MishManners) to find out why its called a doughtnut!
+
+const donut = Number(process.argv[2])
+
+if ((donut % 3 === 0) && (donut % 5 === 0))
+{
+  console.log('FizzBuzz')
+}
+
+
+else if (donut % 3 === 0)
+{
+  console.log('Fizz')
+}
+
+else if (donut % 5 === 0)
+{
+  console.log('Buzz')
+}
+
+else
+{
+  console.log(donut)
+}
