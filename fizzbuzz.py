3688fff6a9be2b944e356ff4c10ac32287738dd0
MishManners <36594527+MishManners@users.noreply.github.com>
2021-02-08 09:30:58 +1100

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,5 @@
 # FizzBuzz, called the constant donut find me on my Twitch channel (MishManners) to find out why its called a doughtnut!
+# You get both spellings; donut and doughnut to make everyone happy!
 # Made this file to show off Code Snippets.
 
 const donut = Number(process.argv[2])
