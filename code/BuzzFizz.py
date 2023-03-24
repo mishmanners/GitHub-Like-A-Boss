e5ff3c15e8a3e3f4eee31476c30c3060db1b2fd6
Michelle Mannering <36594527+MishManners@users.noreply.github.com>
2023-03-24 12:08:23 +1100

    Adding Copilot suggestions

---
+++ code/BuzzFizz.py
@@ -0,0 +1,12 @@
+# This is a python file for fizzbuzz
+
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
--- code/Elevator.js
+++ code/Elevator.js
@@ -1,7 +1,7 @@
 // JavaScript Programming by Magnus Wolffelt and contributors https://play.elevatorsaga.com/
 
 {
-    init: function(elevators, floors) {
+    init : function(elevators, floors) {
         var elevator = elevators[0]; // Let's use the first elevator
 
         // Whenever the elevator is idle (has no more queued destinations) ...
@@ -10,10 +10,10 @@
             elevator.goToFloor(0);
             elevator.goToFloor(1);
             elevator.goToFloor(2);
-            elevator.goToFloor(3);  
+            elevator.goToFloor(3);
         });
     },
-        update: function(dt, elevators, floors) {
-            // We normally don't need to do anything here
-        }
+    update : function(dt, elevators, floors) {
+        // We normally don't need to do anything here
+    }
 }
